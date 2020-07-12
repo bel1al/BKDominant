e49936795c6a133b33a57eb37b503dfc8e1a9831
belial <skywokerua@gmail.com>
2020-07-12 15:08:26 +0300

    Доробив дизайн, пофіксив фон. Потрібно зробити БД

---
+++ src/main/java/com/dominant/finalVariable.java
@@ -0,0 +1,9 @@
+package com.dominant;
+
+public class finalVariable {
+    public static final String TABLE_NAME = "users";
+
+    public static final String USERS_ID = "user_id";
+    public static final String USERS_NAME = "user_name";
+    public static final String USERS_PASSWORD = "user_password";
+}
